7f10fed47667dad3fffb0bfab1fbe651a1e5828d
Juan Esteban Correa <correaguzmanjuanesteban@gmail.com>
2025-04-06 17:02:23 -0500

    no se

---
+++ models/itinerario.py
@@ -0,0 +1,22 @@
+from typing import List
+from models.actividad import Actividad
+from models.entrada_diario import EntradaDiario
+
+class Itinerario:
+    def __init__(self, nombre: str, usuario: str, actividades: list[Actividad], diario: list[EntradaDiario]):
+        self.nombre: str = nombre
+        self.usuario: str = usuario
+        self.actividades: List[Actividad] = actividades
+        self.diario: List[EntradaDiario] = diario
+
+    def agregar_actividad(self, actividad: Actividad) -> None:
+        self.actividades.append(actividad)
+
+    def agregar_entrada_diario(self, entrada: EntradaDiario) -> None:
+        self.diario.append(entrada)
+
+    def __str__(self) -> str:
+        return (f"Itinerario: {self.nombre}\n"
+                f" para {self.usuario}\n"
+                f" | Actividades: {len(self.actividades)}\n"
+                f" | Entradas en Diario: {len(self.diario)}")
