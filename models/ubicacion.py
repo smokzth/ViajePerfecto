f867452db25be35c38ecbfb8995debbe4be53cbc
juan arango <juanesarangocalle9@gmail.com>
2025-04-06 16:26:48 -0500

    se crea la clase Ubicacion con atributos de ciudad, país, latitud y longitud

---
+++ models/ubicacion.py
@@ -0,0 +1,6 @@
+class Ubicacion:
+    def __init__(self, ciudad: str, pais: str, latitud: float, longitud: float):
+        self.ciudad: str = ciudad
+        self.pais: str = pais
+        self.latitud: float = latitud
+        self.longitud: float = longitud
