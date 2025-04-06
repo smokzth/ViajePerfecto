555e6e0523dcea896a674208f9c21f7a7ec85cb4
Juan Pablo Gaviria Franco <francopablo155@gmail.com>
2025-04-06 16:28:21 -0500

    Se implementa la clase EntradaDiario en entrada_diario

---
+++ models/entrada_diario.py
@@ -0,0 +1,10 @@
+class EntradaDiario:
+    def __init__(self, actividad: str, fecha: str, nota: str, calificacion: float, ruta_foto: str):
+        self.actividad: str = actividad
+        self.fecha: str = fecha  #Formato "YYYY-MM-DD" o similar
+        self.nota: str = nota
+        self.calificacion: float = calificacion
+        self.ruta_foto: str = ruta_foto
+
+    def __str__(self) -> str:
+        return f"Entrada para {self.actividad} el {self.fecha}: {self.nota} (Cal: {self.calificacion}, Foto: {self.ruta_foto})"
