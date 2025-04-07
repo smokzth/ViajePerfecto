0a91f28aedb843fbf56680b389438eed92bbefdf
Eliud <eliud2921@gmail.com>
2025-04-07 17:23:37 -0500

    Se agrega la clase VistaConsola con el método mostrar_menu_principal

---
+++ views/__init__.py
@@ -0,0 +1,8 @@
+class VistaConsola:
+    def mostrar_menu_principal(self):
+        print ("\n---BIENVENIDO A VIAJE PERFECTO---")
+        print("1.Crear nuevo usuario")
+        print("2. Ver Itinerarios")
+        print("3. salir")
+
+
