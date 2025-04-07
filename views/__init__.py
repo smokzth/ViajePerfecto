469392157bc2cbfcf4058938d2876ad87b25454f
Eliud <eliud2921@gmail.com>
2025-04-07 18:49:47 -0500

    Se implemento el metodo para solicitar_datos_usuarios

--- views/__init__.py
+++ views/__init__.py
@@ -5,4 +5,11 @@ class VistaConsola:
         print("2. Ver Itinerarios")
         print("3. salir")
 
+    def solicitar_datos_usuarios(selfself):
+        print("\n---Registro de Usuario---")
+        nombre=input("Nombre:")
+        preferencias=input("Preferencias(separedas por coma):").split(",")
+        presupuesto=float(input("Presupusto máximo:"))
+        return nombre,[p.strip()for p in preferencias],presupuesto
+
 
