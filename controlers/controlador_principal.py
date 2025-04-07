dfcd2f7894927f04ae6bac7b061ea3b528c1838b
Juan Pablo Gaviria Franco <francopablo155@gmail.com>
2025-04-07 18:43:45 -0500

    Se implemento la función ejecutar

--- controlers/controlador_principal.py
+++ controlers/controlador_principal.py
@@ -2,3 +2,27 @@ from models.usuario import Usuario
 from models.itinerario import  Itinerario
 from views.vista_consola import VistaConsola
 
+class ControladorPrincipal:
+    def __init__(self):
+        self.vista = VistaConsola()
+        self.usuario = None
+
+    def ejecutar(self):
+        while true:
+            self.vista.mostrar_menu_principal()
+            opcion = input("Seleccione una opción: ")
+
+            if opcion == "1":
+                self.crear_usuario()
+
+            elif opcion == "2":
+                if self.usuario:
+                    self.vista.mostrar_itinerarios(self.usuario.itinerarios)
+                else:
+                    self.vista.mostrar_mensaje("Primero debes crear un usuario.")
+
+            elif opcion == "2":
+                self.vista.mostrar_mensaje("¡Graciar por usar Viaje Perfecto! ")
+                break
+            else:
+                self.vista.mostrar_mensaje("Opción no válida. Intente de nuevo. ")
