2817f6cf682acc7065b7030c36cc6f0d299bc116
Eliud <eliud2921@gmail.com>
2025-04-07 19:43:18 -0500

    Se corrige y se implementa el metodo solicutar_datos_itinerario

--- views/vista_consola.py
+++ views/vista_consola.py
@@ -3,7 +3,8 @@ class VistaConsola:
         print("\n--- BIENVENIDO A VIAJE PERFECTO ---")
         print("1. Crear nuevo usuario")
         print("2. Ver itinerarios")
-        print("3. Salir")
+        print("3. Crear itinerario")
+        print("4. Salir")
 
     def solicitar_datos_usuario(self):
         print("\n--- Registro de Usuario ---")
@@ -12,10 +13,17 @@ class VistaConsola:
         presupuesto = float(input("Presupuesto máximo: "))
         return nombre, [p.strip() for p in preferencias], presupuesto
 
+    def solicutar_datos_itinerario(self):
+        print("\n---Crear Itinerario:---")
+        nombre=input("Nombre del itinerario:")
+        presupuesto=float(input("Presupuesto del itinerario"))
+        return nombre, presupuesto
+
+
     def mostrar_itinerarios(self, itinerarios):
         print("\n--- Itinerarios ---")
         if not itinerarios:
-            print("No hay itinerarios registrados.")
+            print("No hay itinerarios Registrados.")
         else:
             for i, it in enumerate(itinerarios, 1):
                 print(f"{i}. {it.nombre} - Actividades: {len(it.actividades)}")
